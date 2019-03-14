a626c248c776bc90e25828e3106f086a2a4a8857
Quentin Gille <xquentin_gille@hotmail.com>
2019-03-14 16:01:51 +0100

    Added one unit test. Dark navbar added.

---
+++ GilleQuentinCore/ClientApp/src/app/home/home.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { HomeComponent } from "./home.component";
+
+describe("HomeComponent", () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomeComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("Should display a title", async(() => {
+    const titleText = fixture.nativeElement.querySelector('h1').textContent;
+    expect(titleText).toEqual('Hello, world!');
+  }));
+});
